e6d8049b6b215c2e0ef5665f7197158d69b4b287
Dan Head <me@danielhead.com>
2018-02-19 18:00:22 +0000

    Store env vars and reload them when running CRON

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ if [[ "$RESTORE" == "true" ]]; then
   ./restore.sh
 else
   if [ -n "$CRON_TIME" ]; then
-    echo "${CRON_TIME} /backup.sh >> /docker-s3.log 2>&1" > /crontab.conf
+    env | grep -v 'affinity:container' | sed -e 's/^\([^=]*\)=\(.*\)/export \1="\2"/' > /env.sh # Save current environment
+    chmod 755 /env.sh
+    echo "${CRON_TIME} /env.sh && /backup.sh >> /docker-s3.log 2>&1" > /crontab.conf
     /usr/bin/crontab /crontab.conf
     echo "=> Running docker-s3 backups as a cronjob for ${CRON_TIME}"
     exec /usr/sbin/crond -f
